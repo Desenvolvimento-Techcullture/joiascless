e168440c06da77ba9b7434d1cbbf7fbdaccc1030
Clevison Prado Batista <clevison@mac.local>
2026-01-22 09:59:08 -0300

    Inclusao dos dados da empresa

---
+++ src/data/company.js
@@ -0,0 +1,117 @@
+import product1 from "@/assets/product-1.jpg";
+import product2 from "@/assets/product-2.jpg";
+import product3 from "@/assets/product-3.jpg";
+import product4 from "@/assets/product-4.jpg";
+// import heroBanner from "@/assets/hero-banner.jpg";
+// import heroBanner from "@/assets/prod10.png";
+
+export const products = [
+    { "id": 768, "image": "https://joiascless.catalogo.app/fotos/189/189_768_produto.jpg", "name": "ANEL SOLITARIO ZIRCONIA", "price": 129.9, "category": "Anéis", "description": "anel solitario zirconia" },
+    { "id": 117, "image": "https://joiascless.catalogo.app/fotos/189/189_117_produto.jpg", "name": "BRINCO BOLINHA", "price": 34.9, "category": "Brincos", "description": "brinco bolinha" },
+    { "id": 1338, "image": "https://joiascless.catalogo.app/fotos/189/189_1338_produto.jpg", "name": "PULSEIRA 3 CRISTAIS INFANTIL", "price": 89.9, "category": "Pulseiras", "description": "pulseira 3 cristais infantil" },
+    { "id": 1331, "image": "https://joiascless.catalogo.app/fotos/189/189_1331_produto.jpg", "name": "PULSEIRA PEIXE COM PEDRINHA OURO", "price": 89.9, "category": "Pulseiras", "description": "pulseira peixe com pedrinha ouro" },
+    { "id": 1330, "image": "https://joiascless.catalogo.app/fotos/189/189_1330_produto.jpg", "name": "PULSEIRA INFANTIL PEIXINHO OURO", "price": 99.9, "category": "Pulseiras", "description": "pulseira infantil peixinho ouro" },
+    { "id": 1322, "image": "https://joiascless.catalogo.app/fotos/189/189_1322_produto.jpg", "name": "COLAR INFANTIL VESTIDO PRATA", "price": 89.9, "category": "Colares", "description": "colar infantil vestido prata" },
+    { "id": 1321, "image": "https://joiascless.catalogo.app/fotos/189/189_1321_produto.jpg", "name": "COLAR INFANTIL VESTIDO OURO", "price": 154.9, "category": "Colares", "description": "colar infantil vestido ouro" },
+    { "id": 1314, "image": "https://joiascless.catalogo.app/fotos/189/189_1314_produto.jpg", "name": "BRINCO VESTIDO PRATA", "price": 54.9, "category": "Brincos", "description": "brinco vestido prata" },
+    { "id": 1290, "image": "https://joiascless.catalogo.app/fotos/189/189_1290_produto.jpg", "name": "ANEL SOLITARIO STRASS 12", "price": 89.9, "category": "Anéis", "description": "anel solitario strass 12" },
+    { "id": 1278, "image": "https://joiascless.catalogo.app/fotos/189/189_1278_produto.jpg", "name": "PULSEIRA FIGARO 3X1 3MM", "price": 129.9, "category": "Pulseiras", "description": "pulseira figaro 3x1 3mm" },
+    { "id": 1277, "image": "https://joiascless.catalogo.app/fotos/189/189_1277_produto.jpg", "name": "PULSEIRA CARTIER MASCULINA OURO", "price": 169.9, "category": "Pulseiras", "description": "pulseira cartier masculina ouro" },
+    { "id": 1275, "image": "https://joiascless.catalogo.app/fotos/189/189_1275_produto.jpg", "name": "COLAR FIGARO 3X1 14MM", "price": 544.9, "category": "Colares", "description": "colar figaro 3x1 14mm" },
+    { "id": 1274, "image": "https://joiascless.catalogo.app/fotos/189/189_1274_produto.jpg", "name": "COLAR CRUZ MASCULINO OURO", "price": 232.9, "category": "Colares", "description": "colar cruz masculino ouro" },
+    { "id": 1273, "image": "https://joiascless.catalogo.app/fotos/189/189_1273_produto.jpg", "name": "COLAR CRUZ MASCULINO PRATA", "price": 148.9, "category": "Colares", "description": "colar cruz masculino prata" },
+    { "id": 1265, "image": "https://joiascless.catalogo.app/fotos/189/189_1265_produto.jpg", "name": "COLAR PROFISSÕES CABELEREIRA", "price": 159.9, "category": "Colares", "description": "colar profissões cabeleireira" },
+    { "id": 1252, "image": "https://joiascless.catalogo.app/fotos/189/189_1252_produto.jpg", "name": "PULSEIRA CRISTAIS PRATA", "price": 99.9, "category": "Pulseiras", "description": "pulseira cristais prata" },
+    { "id": 1251, "image": "https://joiascless.catalogo.app/fotos/189/189_1251_produto.jpg", "name": "BRINCO GOTA PRATA", "price": 99.9, "category": "Brincos", "description": "brinco gota prata" },
+    { "id": 1244, "image": "https://joiascless.catalogo.app/fotos/189/189_1244_produto.jpg", "name": "COLAR MENINO BONE PRATA", "price": 89.9, "category": "Colares", "description": "colar menino bone prata" },
+    { "id": 1215, "image": "https://joiascless.catalogo.app/fotos/189/189_1215_produto.jpg", "name": "COLAR VAN CLEEF", "price": 154.9, "category": "Colares", "description": "colar van cleef" },
+    { "id": 1212, "image": "https://joiascless.catalogo.app/fotos/189/189_1212_produto.jpg", "name": "PULSEIRA VAN CLEEF", "price": 109.9, "category": "Pulseiras", "description": "pulseira van cleef" },
+    { "id": 1204, "image": "https://joiascless.catalogo.app/fotos/189/189_1204_produto.jpg", "name": "ARGOLA GRANDE CORACAO", "price": 98.9, "category": "Argolas", "description": "argola grande coracao" },
+    { "id": 1145, "image": "https://joiascless.catalogo.app/fotos/189/189_1145_produto.jpg", "name": "PULSEIRA ELO CORAÇÃO GRANDE PRATA", "price": 109.9, "category": "Pulseiras", "description": "pulseira elo coração grande prata" },
+    { "id": 1144, "image": "https://joiascless.catalogo.app/fotos/189/189_1144_produto.jpg", "name": "ARGOLA FLOR COLORIDA", "price": 169.9, "category": "Argolas", "description": "argola flor colorida" },
+    { "id": 1143, "image": "https://joiascless.catalogo.app/fotos/189/189_1143_produto.jpg", "name": "ARGOLINHA CORAÇÃO CRAVEJADO", "price": 119.9, "category": "Argolinhas", "description": "argolinha coração cravejado" },
+    { "id": 1141, "image": "https://joiascless.catalogo.app/fotos/189/189_1141_produto.jpg", "name": "ARGOLINHA CRAVEJADA 2 FILEIRAS 10MM", "price": 109.9, "category": "Argolinhas", "description": "argolinha cravejada 2 fileiras 10mm" },
+    { "id": 1140, "image": "https://joiascless.catalogo.app/fotos/189/189_1140_produto.jpg", "name": "ARGOLINHA CRAVEJADA 2 FILEIRAS 14MM", "price": 129.9, "category": "Argolinhas", "description": "argolinha cravejada 2 fileiras 14mm" },
+    { "id": 1138, "image": "https://joiascless.catalogo.app/fotos/189/189_1138_produto.jpg", "name": "ARGOLINHA CRUZ CRAVEJADA G", "price": 129.9, "category": "Argolinhas", "description": "argolinha cruz cravejada g" },
+    { "id": 1137, "image": "https://joiascless.catalogo.app/fotos/189/189_1137_produto.jpg", "name": "ARGOLINHA CRUZ CRAVEJADA M", "price": 109.9, "category": "Argolinhas", "description": "argolinha cruz cravejada m" },
+    { "id": 1136, "image": "https://joiascless.catalogo.app/fotos/189/189_1136_produto.jpg", "name": "ARGOLINHA CRUZ CRAVEJADA P", "price": 99.9, "category": "Argolinhas", "description": "argolinha cruz cravejada p" },
+    { "id": 1135, "image": "https://joiascless.catalogo.app/fotos/189/189_1135_produto.jpg", "name": "ARGOLINHA QUADRADA 13MM", "price": 89.9, "category": "Argolinhas", "description": "argolinha quadrada 13mm" },
+    { "id": 1133, "image": "https://joiascless.catalogo.app/fotos/189/189_1133_produto.jpg", "name": "BRINCO TREVO CRAVEJADO", "price": 84.9, "category": "Brincos", "description": "brinco trevo cravejado" },
+    { "id": 1129, "image": "https://joiascless.catalogo.app/fotos/189/189_1129_produto.jpg", "name": "COLAR PONTO DE LUZ ZIRCONIA", "price": 139.9, "category": "Colares", "description": "colar ponto de luz zirconia" },
+    { "id": 1125, "image": "https://joiascless.catalogo.app/fotos/189/189_1125_produto.jpg", "name": "ALIANÇA CHANFRADA AÇO", "price": 49.9, "category": "Alianças", "description": "aliança chanfrada aço" },
+    { "id": 1123, "image": "https://joiascless.catalogo.app/fotos/189/189_1123_produto.jpg", "name": "ANEL SOLITARIO AÇO", "price": 49.9, "category": "Anéis", "description": "anel solitario aço" },
+    { "id": 1122, "image": "https://joiascless.catalogo.app/fotos/189/189_1122_produto.jpg", "name": "ARGOLA VAZADA DECORADA", "price": 139.9, "category": "Argolas", "description": "argola vazada decorada" },
+    { "id": 1121, "image": "https://joiascless.catalogo.app/fotos/189/189_1121_produto.jpg", "name": "BRINCO LONGO GOTA", "price": 124.9, "category": "Brincos", "description": "brinco longo gota" },
+    { "id": 1089, "image": "https://joiascless.catalogo.app/fotos/189/189_1089_produto.jpg", "name": "BRINCO ESTRELA FIXO", "price": 44.9, "category": "Brincos", "description": "brinco estrela fixo" },
+    { "id": 1077, "image": "https://joiascless.catalogo.app/fotos/189/189_1077_produto.jpg", "name": "Argolinha Letra Q Strass", "price": 69.9, "category": "Argolinhas", "description": "argolinha letra q strass" },
+    { "id": 1066, "image": "https://joiascless.catalogo.app/fotos/189/189_1066_produto.jpg", "name": "Brinco Trevo Vazado O.", "price": 39.9, "category": "Brincos", "description": "brinco trevo vazado o." },
+    { "id": 1062, "image": "https://joiascless.catalogo.app/fotos/189/189_1062_produto.jpg", "name": "COLAR ESTRELA DE DAVI OURO", "price": 259.9, "category": "Colares", "description": "colar estrela de davi ouro" },
+    { "id": 1061, "image": "https://joiascless.catalogo.app/fotos/189/189_1061_produto.jpg", "name": "COLAR ESCUDO CRUZ OURO", "price": 323.9, "category": "Colares", "description": "colar escudo cruz ouro" },
+    { "id": 1057, "image": "https://joiascless.catalogo.app/fotos/189/189_1057_produto.jpg", "name": "Pulseira Masculina Grumet 7mm", "price": 319.9, "category": "Pulseiras", "description": "pulseira masculina grumet 7mm" },
+    { "id": 1050, "image": "https://joiascless.catalogo.app/fotos/189/189_1050_produto.jpg", "name": "Argolinha Estrela do Mar Banhado a Ouro", "price": 69.9, "category": "Argolinhas", "description": "argolinha estrela do mar banhado a ouro" },
+    { "id": 1042, "image": "https://joiascless.catalogo.app/fotos/189/189_1042_produto.jpg", "name": "COLAR CAUDA SEREIA PRATA", "price": 139.9, "category": "Colares", "description": "colar cauda sereia prata" },
+    { "id": 1033, "image": "https://joiascless.catalogo.app/fotos/189/189_1033_produto.jpg", "name": "ANEL VERSAILLES", "price": 169.9, "category": "Anéis", "description": "anel versailles" },
+    { "id": 1024, "image": "https://joiascless.catalogo.app/fotos/189/189_1024_produto.jpg", "name": "ANEL CARISMA", "price": 113.9, "category": "Anéis", "description": "anel carisma" },
+    { "id": 1023, "image": "https://joiascless.catalogo.app/fotos/189/189_1023_produto.jpg", "name": "PULSEIRA CARISMA", "price": 253.9, "category": "Pulseiras", "description": "pulseira carisma" },
+    { "id": 1020, "image": "https://joiascless.catalogo.app/fotos/189/189_1020_produto.jpg", "name": "BRINCO PRISMA 4MM", "price": 34.9, "category": "Brincos", "description": "brinco prisma 4mm" },
+    { "id": 1018, "image": "https://joiascless.catalogo.app/fotos/189/189_1018_produto.jpg", "name": "TORNOZELEIRA FLORENCE 6MM", "price": 152.9, "category": "Tornozeleiras", "description": "tornozeleira florence 6mm" },
+    { "id": 1016, "image": "https://joiascless.catalogo.app/fotos/189/189_1016_produto.jpg", "name": "CHOCKER FLORENCE 6MM", "price": 189.9, "category": "Chockers", "description": "chocker florence 6mm" }
+]
+
+
+export const data = {
+    name: 'JOIAS CLESS',
+    slogan: 'Semijoias sob medida para você.',
+    address: 'Rua Ivan Colen, 140  - Santa Luzia - MG',
+    phone: '+55 (31) 98270-7263',
+    email: 'joiascless@gmail.com',
+    instagram: 'https://www.instagram.com/joiascless/',
+    facebook: 'https://www.facebook.com/joiascless',
+    whatsapp: 'https://wa.me/5531982707263',
+}
+const currentMonth = new Date().toLocaleString('pt-BR', { month: 'long' });
+
+export const colection = {
+    banner: 'http://sistema.innosystem.com.br/files/logos/189_catalogo.jpg',
+    // banner: heroBanner,
+    title: ``,
+    description: ''
+}
+export const navLinks = [
+    { path: "/", label: "Início" },
+    { path: "/products", label: "Produtos" },
+    { path: "/contact", label: "Contato" },
+];
+
+export const categories = [...new Set(products.map(product => product.category))];
+
+export const sizes = ["PP", "P", "M", "G", "GG", "16", "17", "18", "19", "20", "21", "22"];
+
+// export const sizes = [...new Set(products.map(product => product.size))];
+
+export const theme = {
+    primary: "0 0% 15%;",
+    secondary: "24 10% 95%",
+    accent: "340 65% 75%"
+}
+
+export const availableCities = [
+    "Belo Horizonte",
+    "Contagem",
+    "Ribeirão das Neves",
+    "Santa Luzia",
+    "Vespasiano",
+    "Sabará",
+    "Esmeralda",
+    "Betim",
+    "Ibirite",
+    "Nova Lima",
+    "Pedro Leopoldo",
+    "São José da Lapa",
+    "Lagoa Santa",
+    "Nova Contagem"
+];
+
+export const freeCities = [
+    "Belo Horizonte",
+];
